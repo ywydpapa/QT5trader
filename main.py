25eacc30451214bcc139bf1165a722afd2d76ac6
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2023-04-14 15:23:10 +0900

    전체 조회

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import pykorbit
-import json
+import datetime
 
 
 form_class = uic.loadUiType("mywindow.ui")[0]
@@ -16,22 +16,24 @@ class MyWindow(QMainWindow, form_class):
         self.pushButton_2.clicked.connect(self.btn2_clicked)
 
     def btn_clicked(self):
-        price1 = pykorbit.get_current_price("BTC")
-        price2 = pykorbit.get_current_price("ETH")
-        price3 = pykorbit.get_current_price("FIL")
-        order1 = pykorbit.get_orderbook("BTC")
-        order2 = pykorbit.get_orderbook("ETH")
-        order3 = pykorbit.get_orderbook("FIL")
-        self.textBrowser.append(str(format(price1,",")))
-        self.textBrowser.append(str(order1))
-        self.textBrowser.append(" ")
-        self.textBrowser.append(str(format(price2, ",")))
-        self.textBrowser.append(str(order2))
-        self.textBrowser.append(" ")
-        self.textBrowser.append(str(format(price3, ",")))
-        self.textBrowser.append(str(order3))
-        self.textBrowser.append(" ")
-        self.textBrowser.append("*************************************************")
+        coinlist = pykorbit.get_tickers()
+        for coins in coinlist:
+            self.textBrowser.append(str(coins))
+            price3 = pykorbit.get_current_price(str(coins))
+            order3 = pykorbit.get_orderbook(str(coins))
+            self.textBrowser.append(str(format(price3, ",")))
+            bids = order3["bids"]
+            asks = order3["asks"]
+            timestmp = order3["timestamp"]
+            self.textBrowser.append(str(datetime.datetime.fromtimestamp(timestmp / 1000)))
+            self.textBrowser.append("**************** BIDS ****************************")
+            for bidlist in bids:
+                self.textBrowser.append(str(bidlist))
+            self.textBrowser.append("***************** ASKS **************************")
+            for asklist in asks:
+                self.textBrowser.append(str(asklist))
+            self.textBrowser.append(" ")
+            self.textBrowser.append("*************************************************")
 
     def btn2_clicked(self):
         self.textBrowser.clear()
